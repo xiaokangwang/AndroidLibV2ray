4db31e99e0f691ae6f0697cb29926f460addf689
Shelikhoo <xiaokangwang@outlook.com>
2016-07-14 12:38:02 +0800

    initial code basic binding

---
+++ interact.go
@@ -0,0 +1,61 @@
+package libv2ray
+
+import (
+	// The following are necessary as they register handlers in their init functions.
+	_ "github.com/v2ray/v2ray-core/app/router/rules"
+	"github.com/v2ray/v2ray-core/common/log"
+	"github.com/v2ray/v2ray-core/shell/point"
+
+	// The following are necessary as they register handlers in their init functions.
+	_ "github.com/v2ray/v2ray-core/proxy/blackhole"
+	_ "github.com/v2ray/v2ray-core/proxy/dokodemo"
+	_ "github.com/v2ray/v2ray-core/proxy/freedom"
+	_ "github.com/v2ray/v2ray-core/proxy/http"
+	_ "github.com/v2ray/v2ray-core/proxy/shadowsocks"
+	_ "github.com/v2ray/v2ray-core/proxy/socks"
+	_ "github.com/v2ray/v2ray-core/proxy/vmess/inbound"
+	_ "github.com/v2ray/v2ray-core/proxy/vmess/outbound"
+
+	// The following are necessary as they register handlers in their init functions.
+	_ "github.com/v2ray/v2ray-core/transport/internet/kcp"
+	_ "github.com/v2ray/v2ray-core/transport/internet/tcp"
+	_ "github.com/v2ray/v2ray-core/transport/internet/udp"
+)
+
+/*V2RayPoint V2Ray Point Server
+This is territory of Go, so no getter and setters!
+*/
+type V2RayPoint struct {
+	ConfigureFile string
+	OnEmitStatus  *func(StatusCode int, Status string) int
+}
+
+func (v *V2RayPoint) pointloop() {
+	config, err := point.LoadConfig(v.ConfigureFile)
+	if err != nil {
+		log.Error("Failed to read config file (", v.ConfigureFile, "): ", v.ConfigureFile, err)
+		if v.OnEmitStatus != nil {
+			(*v.OnEmitStatus)(-1, "Failed to read config file ("+v.ConfigureFile+")")
+		}
+		return
+	}
+	if config.LogConfig != nil && len(config.LogConfig.AccessLog) > 0 {
+		log.InitAccessLogger(config.LogConfig.AccessLog)
+	}
+
+	vPoint, err := point.NewPoint(config)
+	if err != nil {
+		log.Error("Failed to create Point server: ", err)
+		if v.OnEmitStatus != nil {
+			(*v.OnEmitStatus)(-1, "Failed to create Point server ("+err.Error()+")")
+		}
+		return
+	}
+	vPoint.Start()
+}
+
+/*RunLoop Run V2Ray main loop
+ */
+func (v *V2RayPoint) RunLoop() {
+	go v.pointloop()
+}
---
+++ libv2ray.go
@@ -0,0 +1,8 @@
+package libv2ray
+
+/*CheckVersion int
+This func will return libv2ray binding version.
+*/
+func CheckVersion() int {
+	return 1
+}
