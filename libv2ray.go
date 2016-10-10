e996d4e24f0f82ddccb0b13aa2cf354f896d300c
Shelikhoo <xiaokangwang@outlook.com>
2016-10-10 19:02:46 +0800

    Improve in version api

--- libv2ray.go
+++ libv2ray.go
@@ -1,8 +1,21 @@
 package libv2ray
 
+import (
+	"fmt"
+
+	"v2ray.com/core"
+)
+
 /*CheckVersion int
 This func will return libv2ray binding version.
 */
 func CheckVersion() int {
-	return 19
+	return 20
+}
+
+/*CheckVersionX string
+This func will return libv2ray binding version and V2Ray version used.
+*/
+func CheckVersionX() string {
+	return fmt.Sprintf("Libv2ray rev. %d, along with V2Ray %s", CheckVersion(), core.Version())
 }
