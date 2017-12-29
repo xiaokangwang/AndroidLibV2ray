b018013dcbebe51fcbeac8b564bdb9b867499f9a
Shelikhoo <xiaokangwang@outlook.com>
2017-12-29 10:58:26 +0800

    More docs

--- assertOverwrite.go
+++ assertOverwrite.go
@@ -2,6 +2,13 @@ package libv2ray
 
 var overridedAssets map[string](string)
 
+/*SetAssetsOverride will define file path @[data] to
+provide the asset named @[path].
+
+SetAssetsOverride("geoip.dat","/data/data/appdir/dat/geoip-override.dat")
+will ask libv2ray to serve v2ray "geoip.dat" with
+file "/data/data/appdir/dat/geoip-override.dat" in file system
+*/
 func SetAssetsOverride(path string, data string) {
 	if overridedAssets == nil {
 		overridedAssets = make(map[string](string))
@@ -9,6 +16,12 @@ func SetAssetsOverride(path string, data string) {
 	overridedAssets[path] = data
 }
 
+/*ClearAssetsOverride can disable an override
+at given @[path].
+
+ClearAssetsOverride("geoip.dat")
+will ask libv2ray to serve vanilla asset file.
+*/
 func ClearAssetsOverride(path string) {
 	if overridedAssets == nil {
 		overridedAssets = make(map[string](string))
